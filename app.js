62b7b7049fae014db2963c481106da4734817d1b
pfellow <pfellow@gmail.com>
2023-06-13 00:07:44 +1200

    added .html and .css files

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const rootDir = require('./utils/path');
 
 const app = express();
 
@@ -7,8 +11,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
+});
 
 app.listen(3000);

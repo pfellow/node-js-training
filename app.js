0b53e5b33b26dc8462e0fb9b5db6d8f1fea9599d
pfellow <pfellow@gmail.com>
2023-06-15 00:19:42 +1200

    added templates (ejs)

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ const rootDir = require('./utils/path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found!' });
 });
 
 app.listen(3000);

dc7920d73395883667c56ef656a8267d25076a97
pfellow <pfellow@gmail.com>
2023-06-15 23:35:01 +1200

    refactored (MVC)

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const rootDir = require('./utils/path');
+const errorsController = require('./controllers/errors');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found!' });
-});
+app.use(errorsController.get404);
 
 app.listen(3000);
---
+++ models/product.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/path');
+
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) cb([]);
+    return cb(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+    });
+  }
+
+  static async fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};

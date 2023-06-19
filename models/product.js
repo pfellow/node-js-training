3b572fd1ba15aec40c6bc1d8262f7a049ad21b11
pfellow <pfellow@gmail.com>
2023-06-19 22:59:54 +1200

    app structure

--- models/product.js
+++ models/product.js
@@ -1,29 +1,40 @@
 const fs = require('fs');
 const path = require('path');
-const rootDir = require('../utils/path');
 
-const p = path.join(rootDir, 'data', 'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
-    if (err) cb([]);
-    return cb(JSON.parse(fileContent));
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
   }
 
-  static async fetchAll(cb) {
+  static fetchAll(cb) {
     getProductsFromFile(cb);
   }
 };
